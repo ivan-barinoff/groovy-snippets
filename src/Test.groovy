85bf9dfe566b25d7513f97fb0706c4e0180bdbf4
Ivan <2good4u.tox@gmail.com>
2016-10-17 00:15:59 +0300

    initial

---
+++ src/Test.groovy
@@ -0,0 +1,13 @@
+class Test {
+    static main(args) {
+        println factorial(5)
+    }
+
+    static factorial(n) {
+        int sum = 1;
+        for (i in 2..n) {
+            sum *= i;
+        }
+        sum
+    }
+}
